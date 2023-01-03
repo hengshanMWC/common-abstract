1616542b71f3d8b32103e772b1f6bee86b6254bc
mawencan <1044926436@qq.com>
2023-01-03 15:45:33 +0800

    feat: 填充内柔

--- index.ts
+++ index.ts
@@ -1,3 +1,17 @@
-export function demo() {
-  return 'demo'
+export interface Task {
+  start: (...params: any) => Promise<any>
+  pause: AnyFunc
+  clear: AnyFunc
+}
+
+export type AnyFunc = (...params: any) => any
+
+export type CurrentResolve = (data: any) => void
+export type CurrentReject = (err: any) => void
+
+export abstract class HoldPromise {
+  protected currentResolve?: CurrentResolve | undefined
+  protected currentReject?: CurrentReject | undefined
+  protected abstract triggerResolve?(value: any): boolean
+  protected abstract triggerReject?(err: any): boolean
 }
